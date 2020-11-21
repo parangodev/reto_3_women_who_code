c891147f28097463a57cc9bc6f04877edbfe5553
Pablo Arango <pabloarangobte@gmail.com>
2020-11-21 07:44:07 -0500

    Add files via upload

---
+++ lineal.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {*} array 
+ * @param {*} number 
+ */
+const linealSearch = (array, number) => {
+    let resp = 0
+    // Write your code here
+
+    return resp
+}
+
+console.log(linealSearch(x,y))
